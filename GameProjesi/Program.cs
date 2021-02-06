a94e5a3f176f35c00269c7467c96e46a616e391a
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-02-06 19:43:40 +0300

    Add project files.

---
+++ GameProjesi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameProjesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
